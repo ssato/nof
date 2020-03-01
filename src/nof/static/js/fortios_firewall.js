01cc31db6a8a5b2d7a8844e1cc0fda4290543310
Satoru SATOH <satoru.satoh@gmail.com>
2020-03-02 02:39:55 +0900

    enhancement: add fortios pages and js code

---
+++ src/nof/static/js/fortios_firewall.js
@@ -0,0 +1,25 @@
+/*
+ * @param {Array} fwcs A mapping object holds firewall configurations
+ */
+function make_firewall_policy_maps(fwcs) {
+    /*
+     * @seealso https://api.jquery.com/jQuery.map/
+     */
+    const keys = ["action", "comments", "dstaddr", "dstintf", "logtraffic",
+                  "name", "schedule", "service", "srcaddr", "srcintf",
+                  "status", "uuid"];
+
+    let policies = $.map(fwcs["policy"], function (val, key) {
+        val["_id"] = key;
+
+        for (let k in keys) {
+            if (val.get(k) == undefined) {
+                val[k] = 'undef'; // default value
+            }
+        }
+        return val;
+    });
+    return policies;
+}
+/* vim:sw=4:ts=4:et:
+ */
