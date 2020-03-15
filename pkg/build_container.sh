14cba12354b9e13286bb4ba9dc868edb5d235799
Satoru SATOH <satoru.satoh@gmail.com>
2020-03-16 05:25:43 +0900

    fix: correct some typos and update Dockerfile and its build script

--- pkg/build_container.sh
+++ pkg/build_container.sh
@@ -2,12 +2,15 @@
 set -ex
 
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-WORKDIR=${1:-$(mktemp -d)}
 
-mkdir -p ${WORKDIR}/container
+BUILD_CONTAINER_IMAGE=${1:-buildah bud}
+WORKDIR=${2:-$(mktemp -d)}
+
+mkdir -p ${WORKDIR}/container/
+cp pkg/requirements.txt ${WORKDIR}/container/
 cp -a src ${WORKDIR}/container/
 sed -r "s/@BUILD_DATE@/$BUILD_DATE/g" pkg/Dockerfile.in > ${WORKDIR}/container/Dockerfile
 cd ${WORKDIR}/container/ && \
-buildah bud -t nof:latest . && \ 
+${BUILD_CONTAINER_IMAGE} -t nof:latest .
 # run this container image
 # podman run -d -p 5000:5000 nof
