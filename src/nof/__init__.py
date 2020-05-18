8dab49fabc489d078a898794cc19129036455ad7
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-18 15:13:49 +0900

    change: move create_app to nof.init

--- src/nof/__init__.py
+++ src/nof/__init__.py
@@ -4,40 +4,10 @@
 #
 r"""Entrypoint of this app.
 """
-import flask
-import flask_bootstrap
-import flask_wtf.csrf
-
+from .init import create_app
 from . import config, main, fortios
 
 
 __version__ = "0.2.0"
-
-
-def create_app(cnf_name="development"):
-    """
-    :param cnf_name: Config name, e.g. "development"
-    """
-    cnf = config.get_config(cnf_name)
-
-    app = flask.Flask(__name__)
-    app.config.from_object(cnf)
-    cnf.init_app(app)
-
-    bootstrap = flask_bootstrap.Bootstrap()
-    bootstrap.init_app(app)
-
-    csrf = flask_wtf.csrf.CSRFProtect()
-    csrf.init_app(app)
-
-    app.register_blueprint(main.APP)
-    app.register_blueprint(main.API)
-
-    # >= 1.1:
-    # app.register_blueprint(fortios.CLI)
-    app.register_blueprint(fortios.APP)
-    app.register_blueprint(fortios.API)
-
-    return app
 
 # vim:sw=4:ts=4:et:
---
+++ src/nof/init.py
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2020 Satoru SATOH <ssato@redhat.com>.
+# SPDX-License-Identifier: MIT
+#
+r"""Initialize this app.
+"""
+import flask
+import flask_bootstrap
+import flask_wtf.csrf
+
+
+def create_app(cnf_name="development"):
+    """
+    :param cnf_name: Config name, e.g. "development"
+    """
+    cnf = config.get_config(cnf_name)
+
+    app = flask.Flask(__name__)
+    app.config.from_object(cnf)
+    cnf.init_app(app)
+
+    bootstrap = flask_bootstrap.Bootstrap()
+    bootstrap.init_app(app)
+
+    csrf = flask_wtf.csrf.CSRFProtect()
+    csrf.init_app(app)
+
+    app.register_blueprint(main.APP)
+    app.register_blueprint(main.API)
+
+    app.register_blueprint(fortios.APP)
+    app.register_blueprint(fortios.API)
+
+    return app
+
+# vim:sw=4:ts=4:et:
