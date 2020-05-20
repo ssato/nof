a3e0695a4da0493ad2479db8f808f73ae8eb3be8
Satoru SATOH <satoru.satoh@gmail.com>
2020-05-21 03:33:08 +0900

    fix: move definitions of globals from .libs to .globals to avoid circular imports

--- src/nof/globals.py
+++ src/nof/globals.py
@@ -6,14 +6,24 @@
 # pylint: disable=invalid-name,unused-import
 """globals.
 """
-from .lib import (
-    NODE_TYPES, NODE_ANY, NODE_HOST, NODE_NET, CONFIG_TYPES
-)
-
 
 PACKAGE = "nof"
 AUTHOR = "Satoru SATOH <ssato@redhat.com>"
 
 NOF_DATA_DIR = "/var/lib/nof"
+
+from fortios_xutils import (  # noqa: F401
+    NODE_TYPES,
+    NODE_ANY, NODE_NET, NODE_HOST, NODE_ROUTER, NODE_SWITCH, NODE_FIREWALL
+)
+
+# File types to upload and process
+FILE_TYPES = (
+    FT_NETWORKS,  # networks (nodes and links) files
+    FT_FORTI_SHOW_CONFIG,  # fortios (fortigate) CLI "show *config" outputs
+) = (
+    "networks",
+    "fortios_show_config",
+)
 
 # vim:sw=4:ts=4:et:
