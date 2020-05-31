7c4a4aafa0cf1873503c6e57c07bd7cc464ec764
Satoru SATOH <satoru.satoh@gmail.com>
2020-06-01 04:18:50 +0900

    fix: add missing test cases for .fortios.v1api

--- tests/fortios/v1api.py
+++ tests/fortios/v1api.py
@@ -2,62 +2,70 @@
 # Copyright (C) 2020 Satoru SATOH <ssato@redhat.com>.
 # SPDX-License-Identifier: MIT
 #
-# pylint: disable=missing-docstring, invalid-name
+# pylint: disable=invalid-name,missing-function-docstring
+""".fortios.v1api test cases
+"""
 import os.path
 
-import nof.fortios.utils as U
+import nof.fortios.v1api as TT
+import nof.utils
 
-from nof.fortios import API_PREFIX
+from nof.fortios.common import API_PREFIX
 from .. import common as C
 
 
-def _url_path(*args):
-    """
-    >>> _url_path("firewall", "a.json")
-    '/fortios/api/v1/firewall/a.json'
-    """
-    return '{}/{}'.format(API_PREFIX, os.path.sep.join(args))
+# .. seealso:: nof.fortios.v1api
+UP_PREFIX = os.path.join(API_PREFIX, "upload/")
+GET_PREFIX = os.path.join(API_PREFIX, "configs/")
 
 
-def _err_msg(resp, *args):
-    return "data: {}{}{}".format(resp.data, os.linesep,
-                                 os.linesep.join(args))
-
-
-class V1_API_10_Simple_TestCase(C.BluePrintTestCaseWithWorkdir):
+class V1_API_10_TestCase(C.BluePrintTestCaseWithWorkdir):
 
     maxDiff = None
-    cleanup = False
 
-    hostname = "foo-1"
-    filenames = ["metadata.json", "firewall_policies.json"]
+    cnf_files = C.list_res_files("forti/show_configs/*.txt")
+    hosts = "fortigate-01 fortigate-02".split()
 
-    def test_10_get_host_configs(self):
-        C.skip_test()  # FIXME
+    def test_10_upload_show_config__wrong_data(self):
+        fname = os.path.basename(__file__)
 
-        hdir = U.host_configs_dir(self.hostname)
-        for fn in self.filenames:
-            C.touch_file(os.path.join(hdir, fn))
+        # .. seealso:: nof.fortios.v1api.UP_PATH
+        rpath = os.path.join(UP_PREFIX, fname)
+        content = open(__file__).read()
+        headers = {"content-type": "text/plain"}
 
-        path = _url_path(self.hostname)
-        resp = self.client.get(path)
+        # API: upload_show_config
+        resp = self.client.post(rpath, data=content, headers=headers)
+        self.assertStatus(resp, 400, resp.data)
 
-        self.assertEqual(resp.status_code, 200,
-                         _err_msg(resp, "path: " + path))
-        self.assertEqual(set(resp.data), set(self.filenames),
-                         resp.data)
+        # API: index
+        resp = self.client.get(API_PREFIX + TT.IDX_PATH)
+        self.assertStatus(resp, 204, resp.data)  # 204: no content
+        self.assertFalse(resp.data)
 
-    def test_50_get_host_config(self):
-        hdir = U.host_configs_dir(self.hostname)
-        for fn in self.filenames:
-            filepath = os.path.join(hdir, fn)
-            C.touch_file(filepath, fn)
+    def test_12_upload_show_config__valid_data(self):
+        for hname, fpath in zip(self.hosts, self.cnf_files):
+            fname = os.path.basename(fpath)
+            rpath = os.path.join(UP_PREFIX, fname)
+            content = open(fpath).read()
+            headers = {"content-type": "text/plain"}
 
-            path = _url_path(self.hostname, fn)
-            resp = self.client.get(path)
+            # API: upload_show_config
+            resp = self.client.post(rpath, data=content, headers=headers)
+            self.assertStatus(resp, 201, resp.data)
 
-            self.assertEqual(resp.status_code, 200,
-                             _err_msg(resp, "path: " + path))
-            self.assertEqual(resp.data, open(filepath, "rb").read())
+            upath = nof.utils.uploaded_filepath(fname, TT.FT_FORTI_SHOW_CONFIG,
+                                                content=content)
+            self.assertTrue(os.path.exists(upath))
+
+            # API: index
+            resp = self.client.get(API_PREFIX + TT.IDX_PATH)
+            self.assert200(resp)
+            self.assertTrue(resp.data)
+
+            # API: get_host_config (1)
+            resp = self.client.get(GET_PREFIX + hname)
+            # self.assertStatus(resp, ?, resp.data)  # FIXME
+            self.assertTrue(resp.data)
 
 # vim:sw=4:ts=4:et:
